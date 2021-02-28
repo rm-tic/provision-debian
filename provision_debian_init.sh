0c8912327fbcd765179f6e2d78d1f947e35d379e
Rodrigo Martins <rodrigomartins.tic@gmail.com>
2021-02-27 21:28:31 -0300

    update playbook

--- provision_debian_init.sh
+++ provision_debian_init.sh
@@ -4,192 +4,193 @@ PLAY_USER="$USER"
 
 function GTERM_LOAD()
 {
-	GTERM_DIR="$HOME/.gterm"
-	GTERM_FILE="$GTERM_DIR/gterm-profile.dconf"
-  	GTERM_RC="$GTERM_DIR/gterm.rc"
-	GTERM_URL="https://raw.githubusercontent.com/rm-tic/provision-ubuntu/master/gterm-profile.dconf"
+   GTERM_DIR="$HOME/.gterm"
+   GTERM_FILE="$GTERM_DIR/gterm-profile.dconf"
+   GTERM_RC="$GTERM_DIR/gterm.rc"
+   GTERM_URL="https://raw.githubusercontent.com/rm-tic/provision-ubuntu/master/gterm-profile.dconf"
 
 
-	if [[ ! -d $GTERM_DIR  || "$(cat $GTERM_RC)" != "0" ]]; then
+   if [[ ! -d $GTERM_DIR  || "$(cat $GTERM_RC)" != "0" ]]; then
 
-		mkdir $GTERM_DIR
-    	echo "0" > $GTERM_RC
+      mkdir $GTERM_DIR
+      echo "0" > $GTERM_RC
 
-		wget -qO $GTERM_FILE $GTERM_URL
+      wget -qO $GTERM_FILE $GTERM_URL
 
-		#DCONF EXPORT
-		#dconf dump /org/gnome/terminal/legacy/profiles:/ > $GTERM_FILE
-		
-		#DCONF IMPORT
-		dconf load /org/gnome/terminal/legacy/profiles:/ < $GTERM_FILE
+      #DCONF EXPORT
+      #dconf dump /org/gnome/terminal/legacy/profiles:/ > $GTERM_FILE
 
-	fi
+      #DCONF IMPORT
+      dconf load /org/gnome/terminal/legacy/profiles:/ < $GTERM_FILE
+
+   fi
 
 }
 
 function CHECK_INSTALL()
 {
 
-	PKG="$(dpkg-query -W -f='${db:Status-Abbrev}' $1 2> /dev/null | cut -c 1-2)"
+   PKG="$(dpkg-query -W -f='${db:Status-Abbrev}' $1 2> /dev/null | cut -c 1-2)"
 
-	if [ "$PKG" = "ii" ]; then
+   if [ "$PKG" = "ii" ]; then
 
-		echo "present"
+      echo "present"
 
-	else
+   else
 
-		echo "absent"
+      echo "absent"
 
-	fi
+   fi
 
 }
 
 function APT_UPDATE()
 {
 
-	if [ "$UPDATE_RC" != "0" ]; then
+   if [ "$UPDATE_RC" != "0" ]; then
 
-		sudo apt-get update -qq
-		UPDATE_RC="0"
+      sudo apt-get update -qq
+      UPDATE_RC="0"
 
-	fi
+   fi
 
 }
 
 function INSTALL()
 {
 
-	GIT_STATUS="$(CHECK_INSTALL git)"
-	PYTHON3_VENV_STATUS="$(CHECK_INSTALL python3-venv)"
-  	PYTHON3_PSUTIL_STATUS="$(CHECK_INSTALL python3-psutil)"
-  	PYTHON3_PIP_STATUS="$(CHECK_INSTALL python3-pip)"
-  	CURL_STATUS="$(CHECK_INSTALL curl)"
+   GIT_STATUS="$(CHECK_INSTALL git)"
+   PYTHON3_VENV_STATUS="$(CHECK_INSTALL python3-venv)"
+   PYTHON3_PSUTIL_STATUS="$(CHECK_INSTALL python3-psutil)"
+   PYTHON3_PIP_STATUS="$(CHECK_INSTALL python3-pip)"
+   CURL_STATUS="$(CHECK_INSTALL curl)"
 
-	echo "Installing Essential Packages..."
-	echo
+   echo "Installing Essential Packages..."
+   echo
 
 
-	if [ "$CURL_STATUS" = "absent" ]; then
+   if [ "$CURL_STATUS" = "absent" ]; then
 
-    APT_UPDATE
-    sudo apt-get install -y curl > /dev/null 2>&1
-		echo ">> curl installed."
+      APT_UPDATE
+      sudo apt-get install -y curl > /dev/null 2>&1
+      echo ">> curl installed."
 
-	else
+   else
 
-		echo ">> curl is already installed."
+      echo ">> curl is already installed."
 
-	fi
+   fi
 
-	if [ "$PYTHON3_VENV_STATUS" = "absent" ]; then
+   if [ "$PYTHON3_VENV_STATUS" = "absent" ]; then
 
-    	APT_UPDATE
-    	sudo apt-get install -y python3-venv > /dev/null 2>&1
-		echo ">> python3-venv installed."
+      APT_UPDATE
+      sudo apt-get install -y python3-venv > /dev/null 2>&1
+      echo ">> python3-venv installed."
 
-	else
+   else
 
-		echo ">> python3-venv is already installed."
+      echo ">> python3-venv is already installed."
 
-	fi
+   fi
 
-	if [ "$PYTHON3_PSUTIL_STATUS" = "absent" ]; then
+   if [ "$PYTHON3_PSUTIL_STATUS" = "absent" ]; then
 
-    	APT_UPDATE
-    	sudo apt-get install -y python3-psutil > /dev/null 2>&1
-		echo ">> python3-psutil installed."
+      APT_UPDATE
+      sudo apt-get install -y python3-psutil > /dev/null 2>&1
+      echo ">> python3-psutil installed."
 
-	else
+   else
 
-		echo ">> python3-psutil is already installed."
+      echo ">> python3-psutil is already installed."
 
-	fi
+   fi
 
-  if [ "$PYTHON3_PIP_STATUS" = "absent" ]; then
+   if [ "$PYTHON3_PIP_STATUS" = "absent" ]; then
 
-    	APT_UPDATE
-    	sudo apt-get install -y python3-pip > /dev/null 2>&1
-		echo ">> python3-pip installed."
+      APT_UPDATE
+      sudo apt-get install -y python3-pip > /dev/null 2>&1
+      echo ">> python3-pip installed."
 
-	else
+   else
 
-		echo ">> python3-pip is already installed."
+      echo ">> python3-pip is already installed."
 
-	fi
+   fi
 
-	if [ "$GIT_STATUS" = "absent" ]; then
+   if [ "$GIT_STATUS" = "absent" ]; then
 
-		APT_UPDATE
-    	sudo apt-get install -y git > /dev/null 2>&1
-		echo ">> Git v$(git --version | awk '{print $3}') installed."
+      APT_UPDATE
+      sudo apt-get install -y git > /dev/null 2>&1
+      echo ">> Git v$(git --version | awk '{print $3}') installed."
 
-	else
+   else
 
-		echo ">> Git is already installed."
+      echo ">> Git is already installed."
 
-	fi
+   fi
 
-	echo
+   echo
 
 }
 
 function CLONE_REPO()
 {
-	REPO_DIR="/tmp/provision-ubuntu"
-	REPO_URL="https://github.com/rm-tic/provision-ubuntu.git"
+   REPO_DIR="/tmp/provision-ubuntu"
+   REPO_URL="https://github.com/rm-tic/provision-ubuntu.git"
 
-	echo "Cloning Repository in $REPO_DIR"
+   echo "Cloning Repository in $REPO_DIR"
 
-	git clone $REPO_URL $REPO_DIR > /dev/null 2>&1
+   git clone $REPO_URL $REPO_DIR > /dev/null 2>&1
 
-	echo
+   echo
 }
 
 function CREATE_VENV()
 {
-	python3 -m venv "$REPO_DIR/.venv"
+   python3 -m venv "$REPO_DIR/.venv"
 }
 
 function ENABLE_VENV()
 {
-	. "$REPO_DIR/.venv/bin/activate"
+   . "$REPO_DIR/.venv/bin/activate"
 }
 
 function SETUP_VENV()
 {
-	python3 -m pip install --upgrade pip > /dev/null 2>&1
-	python3 -m pip install ansible > /dev/null 2>&1
+   python3 -m pip install --upgrade pip > /dev/null 2>&1
+   python3 -m pip install ansible > /dev/null 2>&1
 }
 
 function EXEC_ANSIBLE()
 {
-	echo
-	echo "Starting Playbook..."
-	echo
-  	ansible-playbook -i "$REPO_DIR/hosts" "$REPO_DIR/main.yml"
+   echo
+   echo "Starting Playbook..."
+   echo
+   ansible-playbook -i "$REPO_DIR/hosts" "$REPO_DIR/main.yml"
 }
 
 function MAIN()
 {
 
-echo
-echo "+----------------------------------+"
-echo "| Invencible (Ansible)             |"
-echo "| Project: provision-ubuntu        |"
-echo "| Author: Rodrigo Martins (IceTux) |"
-echo "| Creation Date: 2021-02-24        |"
-echo "+----------------------------------+"
-echo
-echo
+   echo
+   echo "+------------------------------------+"
+   echo "| Invencible (Ansible)               |"
+   echo "|------------------------------------|"
+   echo "| Project : provision-debian         |"
+   echo "| Author  : Rodrigo Martins (IceTux) |"
+   echo "| Updated : 2021-02-27               |"
+   echo "+------------------------------------+"
+   echo
+   echo
 
-#Exec Functions
-GTERM_LOAD
-INSTALL
-CLONE_REPO
-CREATE_VENV
-ENABLE_VENV
-SETUP_VENV
-EXEC_ANSIBLE
+   #Exec Functions
+   GTERM_LOAD
+   INSTALL
+   CLONE_REPO
+   CREATE_VENV
+   ENABLE_VENV
+   SETUP_VENV
+   EXEC_ANSIBLE
 
 }
 
