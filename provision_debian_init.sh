ddd8b74c555ec6a7b53fc3689060abcc7596bd2f
Rodrigo Martins <rodrigo.martins@cortex-intelligence.com>
2021-07-10 13:12:11 -0300

    update playbook

--- provision_debian_init.sh
+++ provision_debian_init.sh
@@ -1,198 +1,147 @@
 #!/bin/bash
 
-PLAY_USER="$USER"
-
-function GTERM_LOAD()
-{
-   GTERM_DIR="$HOME/.gterm"
-   GTERM_FILE="$GTERM_DIR/gterm-profile.dconf"
-   GTERM_RC="$GTERM_DIR/gterm.rc"
-   GTERM_URL="https://raw.githubusercontent.com/rm-tic/provision-debian/main/gterm-profile.dconf"
+# Author: Rodrigo Martins
+# E-mail: rodrigomartins.tic@gmail.com
 
 
-   if [[ ! -d $GTERM_DIR  || "$(cat $GTERM_RC)" != "0" ]]; then
+function gterm_load(){
+  GTERM_DIR="$HOME/.gterm"
+  GTERM_FILE="$GTERM_DIR/gterm-profile.dconf"
+  GTERM_RC="$GTERM_DIR/gterm.rc"
+  GTERM_URL="https://raw.githubusercontent.com/rm-tic/provision-debian/main/gterm-profile.dconf"
 
-      mkdir $GTERM_DIR
-      echo "0" > $GTERM_RC
 
-      wget -qO $GTERM_FILE $GTERM_URL
+  if [[ ! -d $GTERM_DIR  || "$(cat $GTERM_RC)" != "0" ]]; then
+    mkdir $GTERM_DIR
+    echo "0" > $GTERM_RC
+    wget -qO $GTERM_FILE $GTERM_URL
 
-      #DCONF EXPORT
-      #dconf dump /org/gnome/terminal/legacy/profiles:/ > $GTERM_FILE
+    # DCONF Export
+    #dconf dump /org/gnome/terminal/legacy/profiles:/ > $GTERM_FILE
 
-      #DCONF IMPORT
-      dconf load /org/gnome/terminal/legacy/profiles:/ < $GTERM_FILE
-
-   fi
-
+    # DCONF Import
+    dconf load /org/gnome/terminal/legacy/profiles:/ < $GTERM_FILE
+  fi
 }
 
-function CHECK_INSTALL()
-{
+function packages_status(){
+  PACKAGE="$1"
+  PACKAGE_STATUS="$(dpkg-query -W -f='${db:Status-Abbrev}' $PACKAGE 2> /dev/null | cut -c 1-2)"
 
-   PKG="$(dpkg-query -W -f='${db:Status-Abbrev}' $1 2> /dev/null | cut -c 1-2)"
-
-   if [ "$PKG" = "ii" ]; then
-
-      echo "present"
-
-   else
-
-      echo "absent"
-
-   fi
-
+  if [ "$PACKAGE_STATUS" = "ii" ]; then
+    echo "present"
+  else
+    echo "absent"
+  fi
 }
 
-function APT_UPDATE()
-{
-
-   if [ "$UPDATE_RC" != "0" ]; then
-
-      sudo apt-get update -qq
-      UPDATE_RC="0"
-
-   fi
-
+function apt_update(){
+  if [ "$UPDATE_RC" != "0" ]; then
+    $sh_c 'apt-get update -qq'
+    UPDATE_RC="0"
+  fi
 }
 
-function INSTALL()
-{
+function packages_setup(){
 
-   GIT_STATUS="$(CHECK_INSTALL git)"
-   PYTHON3_VENV_STATUS="$(CHECK_INSTALL python3-venv)"
-   PYTHON3_PSUTIL_STATUS="$(CHECK_INSTALL python3-psutil)"
-   PYTHON3_PIP_STATUS="$(CHECK_INSTALL python3-pip)"
-   CURL_STATUS="$(CHECK_INSTALL curl)"
+  GIT_STATUS="$(packages_status git)"
+  PYTHON3_VIRTUALENV_STATUS="$(packages_status python3-virtualenv)"
+  CURL_STATUS="$(packages_status curl)"
 
-   echo "Installing Essential Packages..."
-   echo
+  echo ">>> Installing Essential Packages..."
 
+  if [ "$CURL_STATUS" = "absent" ]; then
+    apt_update
+    $sh_c 'apt-get install -y curl > /dev/null 2>&1'
+    echo ">>> Essential Packages: curl installed."
+  else
+    echo ">>> Essential Packages: curl is already installed."
+  fi
 
-   if [ "$CURL_STATUS" = "absent" ]; then
+  if [ "$PYTHON3_VIRTUALENV_STATUS" = "absent" ]; then
+    apt_update
+    $sh_c 'apt-get install -y python3-virtualenv > /dev/null 2>&1'
+    echo ">>> Essential Packages: python3-virtualenv installed."
+  else
+    echo ">>> Essential Packages: python3-virtualenv is already installed."
+  fi
 
-      APT_UPDATE
-      sudo apt-get install -y curl > /dev/null 2>&1
-      echo ">> curl installed."
-
-   else
-
-      echo ">> curl is already installed."
-
-   fi
-
-   if [ "$PYTHON3_VENV_STATUS" = "absent" ]; then
-
-      APT_UPDATE
-      sudo apt-get install -y python3-venv > /dev/null 2>&1
-      echo ">> python3-venv installed."
-
-   else
-
-      echo ">> python3-venv is already installed."
-
-   fi
-
-   if [ "$PYTHON3_PSUTIL_STATUS" = "absent" ]; then
-
-      APT_UPDATE
-      sudo apt-get install -y python3-psutil > /dev/null 2>&1
-      echo ">> python3-psutil installed."
-
-   else
-
-      echo ">> python3-psutil is already installed."
-
-   fi
-
-   if [ "$PYTHON3_PIP_STATUS" = "absent" ]; then
-
-      APT_UPDATE
-      sudo apt-get install -y python3-pip > /dev/null 2>&1
-      echo ">> python3-pip installed."
-
-   else
-
-      echo ">> python3-pip is already installed."
-
-   fi
-
-   if [ "$GIT_STATUS" = "absent" ]; then
-
-      APT_UPDATE
-      sudo apt-get install -y git > /dev/null 2>&1
-      echo ">> Git v$(git --version | awk '{print $3}') installed."
-
-   else
-
-      echo ">> Git is already installed."
-
-   fi
-
-   echo
-
+  if [ "$GIT_STATUS" = "absent" ]; then
+    apt_update
+    $sh_c 'apt-get install -y git > /dev/null 2>&1'
+    echo ">>> Essential Packages: git installed."
+    echo
+  else
+    echo ">>> Essential Packages: git is already installed."
+    echo
+  fi
 }
 
-function CLONE_REPO()
-{
+function github_clone_repo(){
    REPO_DIR="/tmp/provision-debian"
    REPO_URL="https://github.com/rm-tic/provision-debian.git"
 
-   echo "Cloning Repository in $REPO_DIR"
-
-   git clone $REPO_URL $REPO_DIR > /dev/null 2>&1
-
-   echo
+  echo ">>> Cloning repository in $REPO_DIR..."
+  echo
+  git clone $REPO_URL $REPO_DIR > /dev/null 2>&1
 }
 
-function CREATE_VENV()
-{
-   python3 -m venv "$REPO_DIR/.venv"
+function python_venv_create(){
+  echo ">>> Creating python3 virtualenv..."
+  python3 -m virtualenv -q "$REPO_DIR/.venv"
 }
 
-function ENABLE_VENV()
-{
-   . "$REPO_DIR/.venv/bin/activate"
+function python_venv_activate(){
+  echo ">>> Enabling virtualenv..."
+  echo
+  source "$REPO_DIR/.venv/bin/activate"
 }
 
-function SETUP_VENV()
-{
-   python3 -m pip install --upgrade pip > /dev/null 2>&1
-   python3 -m pip install ansible > /dev/null 2>&1
+function python_venv_setup(){
+  echo ">>> Installing playbook dependencies via pip..."
+  echo
+  python3 -m pip -q install -U -r "$REPO_DIR/requirements.txt"
 }
 
-function EXEC_ANSIBLE()
-{
-   echo
-   echo "Starting Playbook..."
-   echo
-   ansible-playbook -i "$REPO_DIR/hosts" "$REPO_DIR/main.yml"
+function ansible_collections(){
+  echo ">>> Ansible: Install community.general collection"
+  echo
+  ansible-galaxy collection install community.general > /dev/null 2>&1
 }
 
-function MAIN()
-{
-
-   echo
-   echo "+------------------------------------+"
-   echo "| Invencible (Ansible)               |"
-   echo "|------------------------------------|"
-   echo "| Project : provision-debian         |"
-   echo "| Author  : Rodrigo Martins (IceTux) |"
-   echo "| Updated : 2021-02-27               |"
-   echo "+------------------------------------+"
-   echo
-   echo
-
-   #Exec Functions
-   GTERM_LOAD
-   INSTALL
-   CLONE_REPO
-   CREATE_VENV
-   ENABLE_VENV
-   SETUP_VENV
-   EXEC_ANSIBLE
-
+function ansible_run(){
+  echo ">>> Ansible: Starting playbook..."
+  echo
+  ansible-playbook "$REPO_DIR/main.yml"
 }
 
+function main(){
 
-MAIN
+  if [ "$(id -un)" = "root" ]; then
+    sh_c="sh -c"
+  else
+    sh_c="sudo sh -c"
+  fi
+
+  echo
+  echo "+----------------------------------+"
+  echo "| Invencible (Ansible)             |"
+  echo "| Project: provision-debian        |"
+  echo "| Author: Rodrigo Martins (IceTux) |"
+  echo "| Creation Date: 2021-07-09        |"
+  echo "+----------------------------------+"
+  echo
+  echo
+
+  # Load Functions
+  gterm_load
+  packages_setup
+  github_clone_repo
+  python_venv_create
+  python_venv_activate
+  python_venv_setup
+  ansible_collections
+  ansible_run
+}
+
+main
--- roles/config/files/aliases.zsh
+++ roles/config/files/aliases.zsh
@@ -1,35 +1,44 @@
-#GENERAL
+# ZSH THEME
+ZSH_THEME="powerlevel10k/powerlevel10k"
+
+# MANPAGE THEME
+export LESS_TERMCAP_mb=$'\e[1;31m'
+export LESS_TERMCAP_md=$'\e[1;33m'
+export LESS_TERMCAP_so=$'\e[01;44;37m'
+export LESS_TERMCAP_us=$'\e[01;37m'
+export LESS_TERMCAP_me=$'\e[0m'
+export LESS_TERMCAP_se=$'\e[0m'
+export LESS_TERMCAP_ue=$'\e[0m'
+export GROFF_NO_SGR=1
+
+# GENERAL
 export EDITOR="vim"
 
-#HISTORY
-alias history='history -i'
+# HISTORY
+alias history='history -i 1+'
 HISTSIZE=10000
 SAVEHIST=20000
 HISTFILE=~/.zhistory
 
-#MANPAGE THEME
-export LESS_TERMCAP_mb=$'\e[1;31m'     
-export LESS_TERMCAP_md=$'\e[1;33m'     
-export LESS_TERMCAP_so=$'\e[01;44;37m' 
-export LESS_TERMCAP_us=$'\e[01;37m'    
-export LESS_TERMCAP_me=$'\e[0m'        
-export LESS_TERMCAP_se=$'\e[0m'        
-export LESS_TERMCAP_ue=$'\e[0m'        
-export GROFF_NO_SGR=1
-
-#ALIAS
+# ALIAS
 alias pbcopy='xclip -selection clipboard' #Depends xclip package
 alias pbpaste='xclip -selection clipboard -o' #Depends xclip package
 alias ssh='ssh -o ServerAliveInterval=15'
 alias crontab='VISUAL=vi crontab'
 alias git='LANG="en_US" git'
 alias get-ip='dig @8.8.8.8 +short'
-alias get-ipub='curl ifconfig.so'
+alias get-public-ip='curl ifconfig.so'
 
-#FUNCTIONS
+# FUNCTIONS
 ss() { /bin/ss $@ | column -t ;}
 
-#TMUX
+getResolution(){
+while read -r file ; do
+    ffprobe -v quiet -show_format -show_streams -print_format json $file | jq -r '. | .format.filename,.streams[0].height' | tr '\n' ';' | awk -F';' '{print $1": "$2"p"}'
+done <<< $(ls -1 *.ts) | grep -P '[0-9]+p'
+}
+
+# TMUX
 t2() { tmux new-session\; split-window -v -p 50\; select-pane -t0 ;}
 
 t3() { tmux new-session\; split-window -v -p 66\; split-window -v\; select-pane -t0 ;}
